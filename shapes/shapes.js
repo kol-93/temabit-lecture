3981c97a2bd5e8dd278df8ff7558e9e575e54585
kol-93 <kol_93@ukr.net>
2019-12-17 20:06:33 +0200

    temp

--- shapes/shapes.js
+++ shapes/shapes.js
@@ -72,67 +72,132 @@ class AShape {
 	isPointInStroke(context, x, y) {
 		throw new NotImplemented(this.constructor.name + '.isPointInStroke');
 	}
+
+	move(deltaX, deltaY) {
+		throw new NotImplemented(this.constructor.name + '.move');
+	}
+
+	scale(factor, centerX, centerY) {
+		throw new NotImplemented(this.constructor.name + '.scale');
+	}
+
 }
 
-class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Triangle extends AShape {
-	constructor(name, points) {
-		super(name);
-		this.pointA = points[0];
-		this.pointB = points[1];
-		this.pointC = points[2];
-	}
-
-	get pointA() {
-		return this._pointA;
-	}
-
-	get pointB() {
-		return this._pointB;
-	}
-
-	get pointC() {
-		return this._pointC;
-	}
-
-	set pointA(value) {
-		if (!(value instanceof Point)) {
-			throw new TypeError('value must be instance of Point');
-		}
-		this._pointA = value;
-	}
-
-	set pointB(value) {
-		if (!(value instanceof Point)) {
-			throw new TypeError('value must be instance of Point');
-		}
-		this._pointB = value;
-	}
-
-	set pointC(value) {
-		if (!(value instanceof Point)) {
-			throw new TypeError('value must be instance of Point');
-		}
-		this._pointC = value;
+class Shape1 extends AShape {
+	constructor() {
+		super();
+		this.radius = 50;
+		this.centerX = 0;
+		this.centerY = 0;
+		/// radius = 50
+		/// centerX = 0
+		/// centerY = 0
 	}
 
 	get area() {
-		/// (x1, y1), (x2, y2), (x3, y3)
+		return Math.PI * Math.pow(this.radius, 2);
+	}
 
-		// v1: (x2-x1, y2-y1)
-		// v2: (x3-x1, y3-y1)
+	get perimeter() {
+		return 2 * Math.PI * this.radius;
+	}
 
-		// v1 * v2 = (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
-		// Math.abs((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)) / 2
+	draw(context) {
+		context.beginPath();
+		context.moveTo(this.centerX, this.centerY);
+		context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI);
+		context.fill();
+	}
+
+	isPointInShape(context, x, y) {
+		return Math.pow(x - this.centerX, 2) + Math.pow(y - this.centerY, 2) < Math.pow(this.radius, 2);
+	}
+
+	isPointInStroke(context, x, y) {
+		return Math.pow(x - this.centerX, 2) + Math.pow(y - this.centerY, 2) === Math.pow(this.radius, 2);
+	}
+
+	move(deltaX, deltaY) {
+		this.centerX += deltaX;
+		this.centerY += deltaY;
+	}
+
+	scale(factor, centerX, centerY) {
+		this.move(-centerX, -centerY);
+		this.radius *= factor;
+		this.centerX *= factor;
+		this.centerY *= factor;
+		this.move(centerX, centerY);
 	}
 }
 
-class Class {
+// const shape = new Shape1();
+// shape.scale(4, 100, 0);
+// console.log(shape.centerX, shape.centerY);
+// console.log(shape.radius);
 
-}
+
+//
+//
+// class Point {
+// 	constructor(x, y) {
+// 		this.x = x;
+// 		this.y = y;
+// 	}
+// }
+//
+// class Triangle extends AShape {
+// 	constructor(name, points) {
+// 		super(name);
+// 		this.pointA = points[0];
+// 		this.pointB = points[1];
+// 		this.pointC = points[2];
+// 	}
+//
+// 	get pointA() {
+// 		return this._pointA;
+// 	}
+//
+// 	get pointB() {
+// 		return this._pointB;
+// 	}
+//
+// 	get pointC() {
+// 		return this._pointC;
+// 	}
+//
+// 	set pointA(value) {
+// 		if (!(value instanceof Point)) {
+// 			throw new TypeError('value must be instance of Point');
+// 		}
+// 		this._pointA = value;
+// 	}
+//
+// 	set pointB(value) {
+// 		if (!(value instanceof Point)) {
+// 			throw new TypeError('value must be instance of Point');
+// 		}
+// 		this._pointB = value;
+// 	}
+//
+// 	set pointC(value) {
+// 		if (!(value instanceof Point)) {
+// 			throw new TypeError('value must be instance of Point');
+// 		}
+// 		this._pointC = value;
+// 	}
+//
+// 	get area() {
+// 		/// (x1, y1), (x2, y2), (x3, y3)
+//
+// 		// v1: (x2-x1, y2-y1)
+// 		// v2: (x3-x1, y3-y1)
+//
+// 		// v1 * v2 = (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
+// 		// Math.abs((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)) / 2
+// 	}
+// }
+//
+// class Class {
+//
+// }
